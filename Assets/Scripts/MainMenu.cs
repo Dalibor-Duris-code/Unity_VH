563d57b8f728197a5f3123906264048f9354948a
F0REST5 <p.hajso562@gmail.com>
2023-10-28 14:02:10 +0200

    MainMenu buttons impl

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,10 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     // Start is called before the first frame update
+    public void StartGame(string startscene)
+    {
+        SceneManager.LoadScene(startscene);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
     void Start()
     {
         
