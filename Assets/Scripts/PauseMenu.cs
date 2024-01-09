5ed09c97bcdaa53a33a0bbda7c91fac41ce48c9f
DaliborDuris <durisd123@gmail.com>
2024-01-09 10:40:32 +0100

    Update level1, 2

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,18 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static bool GameIsPaused;
+    public GameObject pauseMenuUI;
+    public GameObject player1;
+    public GameObject player2;
+
+    private void Start()
     {
-        
+        GameIsPaused = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("Esc press");
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
     }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+/*
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void LoadGame()
+    {
+        SaveLoadPosition.LoadPosition(player1,player2,"save");
+    }
+
+    public void SaveGame()
+    {
+        SaveLoadPosition.SavePosition(player1, player2,"save");
+    }
+*/
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+    
 }
